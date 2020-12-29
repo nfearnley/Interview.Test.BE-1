59df568d015eb2aac513a67f826a605c9c77d6f3
Natalie Fearnley <nfearnley@gmail.com>
2020-12-29 04:07:37 -0500

    Added setter to Course.Credits

--- GraduationTracker/GraduationTracker/Course.cs
+++ GraduationTracker/GraduationTracker/Course.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int Mark { get; set; }
-        public int Credits { get; }
+        public int Credits { get; set; }
     }
 }
