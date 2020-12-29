080281ea3fa66ef07f59463169c8c04905ede5f4
Natalie Fearnley <nfearnley@gmail.com>
2020-12-29 04:08:40 -0500

    Updated GraduationTracker

--- GraduationTracker/GraduationTracker/GraduationTracker.cs
+++ GraduationTracker/GraduationTracker/GraduationTracker.cs
@@ -1,64 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GraduationTracker
 {
     public partial class GraduationTracker
     {
-        public Tuple<bool, STANDING> HasGraduated(Diploma diploma, Student student)
+        // In order to pass a requirement:
+        // 1. a student must have gained enough credits from the list of required course
+        // 2. AND each of those courses must have been passed with the specified minimum mark
+        private bool CoursesMeetRequirement(int requirementId, Course[] courses)
         {
-            var credits = 0;
-            var average = 0;
-
-            for (int i = 0; i < diploma.Requirements.Length; i++)
+            var requirement = Repository.GetRequirement(requirementId);
+            var requirementCredits = 0;
+            foreach (var courseId in requirement.Courses)
             {
-                for (int j = 0; j < student.Courses.Length; j++)
+                var course = courses.FirstOrDefault(c => c.Id == courseId);
+                if (course != null && course.Mark >= requirement.MinimumMark)
                 {
-                    var requirement = Repository.GetRequirement(diploma.Requirements[i]);
-
-                    for (int k = 0; k < requirement.Courses.Length; k++)
-                    {
-                        if (requirement.Courses[k] == student.Courses[j].Id)
-                        {
-                            average += student.Courses[j].Mark;
-                            if (student.Courses[j].Mark > requirement.MinimumMark)
-                            {
-                                credits += requirement.Credits;
-                            }
-                        }
-                    }
+                    requirementCredits += course.Credits;
                 }
             }
+            return requirementCredits >= requirement.Credits;
+        }
 
+        public Tuple<bool, STANDING> HasGraduated(Diploma diploma, Student student)
+        {
+            // Ensure that all requirements are met
+            var meetsRequirements = diploma.Requirements.All(rId => CoursesMeetRequirement(rId, student.Courses));
+
+            // Calculate average grade and total credits
+            var credits = 0;
+            var average = 0;
+            foreach (var course in student.Courses)
+            {
+                // Only passed courses count toward total credits
+                if (course.Mark >= 50)
+                {
+                    credits += course.Credits;
+                }
+                // All courses count toward average
+                average += course.Mark;
+            }
             average = average / student.Courses.Length;
 
             var standing = STANDING.None;
-
-            if (average < 50)
-                standing = STANDING.Remedial;
-            else if (average < 80)
-                standing = STANDING.Average;
-            else if (average < 95)
-                standing = STANDING.MagnaCumLaude;
-            else
-                standing = STANDING.MagnaCumLaude;
-
-            switch (standing)
+            if (average >= 100)
             {
-                case STANDING.Remedial:
-                    return new Tuple<bool, STANDING>(false, standing);
-                case STANDING.Average:
-                    return new Tuple<bool, STANDING>(true, standing);
-                case STANDING.SumaCumLaude:
-                    return new Tuple<bool, STANDING>(true, standing);
-                case STANDING.MagnaCumLaude:
-                    return new Tuple<bool, STANDING>(true, standing);
-                default:
-                    return new Tuple<bool, STANDING>(false, standing);
+                standing = STANDING.SumaCumLaude;
             }
+            else if (average >= 80)
+            {
+                standing = STANDING.MagnaCumLaude;
+            }
+            else if (average >= 50)
+            {
+                standing = STANDING.Average;
+            }
+            else
+            {
+                standing = STANDING.Remedial;
+            }
+
+            // In order to graduate a student must:
+            // 1. have the minimum average grade (>=50)
+            // 2. AND have met all of the specified requirements
+            // 3. AND have earned the minimum number of credits
+            var graduated = (
+                standing != STANDING.Remedial
+                && meetsRequirements
+                && credits >= diploma.Credits
+            );
+
+            return new Tuple<bool, STANDING>(graduated, standing);
         }
     }
 }
